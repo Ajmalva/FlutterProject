8278b711464ce87fd8d24fa4f8c861b37d10f847
Ajmalva <ajmal.va2000@gmail.com>
2021-06-04 23:41:40 +0530

    Day3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,22 +6,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TopBar( subtitle: "Developer", 
-                title: "Ajmal VA", 
-                color: Color(0xff0b3d2e)),
-                const SizedBox(height: 10),
-                Text('Photography' ,
-                 style: TextStyle(fontSize: 22, color: Color(0xff0b3d2e)),)
-
-          
-              ],
-            )
-                )
-                );
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TopBar(
+                title: 'Ajmal VA',
+                subtitle: 'Developer',
+                color: Color(0xff0B3D2E),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Photograpghy',
+                style: TextStyle(fontSize: 22, color: Color(0xff0B3D2E)),
+              ),
+              const SizedBox(height: 10),
+              GridView.builder(
+                padding: EdgeInsets.all(10),
+                itemCount: 4,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2),
+                itemBuilder: (ctx, index) => Container(
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1622774161048-863b17ed0d8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
