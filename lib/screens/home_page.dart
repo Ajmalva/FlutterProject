869db2f0a5e47a17014d244831b594fea2d469ec
Ajmalva <ajmal.va2000@gmail.com>
2021-06-06 16:32:44 +0530

    day4

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,40 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app0/model/photos_model.dart';
 import 'widgets/top_bar.dart';
+import 'package:flutter_app0/env/keys.dart' as config;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PhotosModel> _photosData = [];
+
+  Future<void> _fetchPhotos() async {
+    final _dioInstance = Dio();
+
+    _dioInstance.options.headers['Authorization'] =
+        "Client-ID ${config.unsplashkey}";
+
+    final _fetchData =
+        await _dioInstance.get('https://api.unsplash.com/photos');
+
+    for (var _item in _fetchData.data) {
+      setState(() {
+        _photosData.add(
+            PhotosModel(id: _item['id'], imgURL: _item['urls']['regular']));
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _fetchPhotos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +56,7 @@ class HomePage extends StatelessWidget {
               const SizedBox(height: 10),
               GridView.builder(
                 padding: EdgeInsets.all(10),
-                itemCount: 4,
+                itemCount: _photosData.length,
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -33,7 +65,7 @@ class HomePage extends StatelessWidget {
                     crossAxisCount: 2),
                 itemBuilder: (ctx, index) => Container(
                   child: Image.network(
-                    'https://images.unsplash.com/photo-1622774161048-863b17ed0d8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+                    _photosData[index].imgURL,
                     fit: BoxFit.cover,
                   ),
                 ),
